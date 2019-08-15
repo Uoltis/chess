a88be3031a06c905acc568dc9dbc578bf4a56d87
Uoltis <rufenik@gmail.com>
2019-08-15 22:29:14 +0300

    Autoresize app

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,10 @@
 export default {
   type: Phaser.AUTO,
   parent: "phaser-example",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
   width: 800,
   height: 800
 };
