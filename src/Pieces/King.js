c9112f361215969b0ce149931cdac0748faa9850
Uoltis <rufenik@gmail.com>
2019-08-27 20:06:30 +0300

    Queen and King moves

--- src/Pieces/King.js
+++ src/Pieces/King.js
@@ -14,6 +14,30 @@ export default class King extends Piece {
   }
 
   showCorrectCell(){
-    
+    var x = this.boardX;
+    var y = this.boardY;
+
+    var cells = [
+      [x + 1, y + 1],
+      [x + 1, y],
+      [x + 1, y - 1],
+      [x, y - 1],
+      [x, y + 1],
+      [x - 1, y + 1],
+      [x - 1, y],
+      [x - 1, y - 1],
+    ]
+
+    cells.forEach(el=>{
+      if (el[0] >= 0 && el[0] <= 7 && el[1] >= 0 && el[1] <= 7) {
+        if(this.isPiece(el[0], el[1])){
+          if(!this.isAlly(el[0], el[1])){
+            this.board[el[0]][el[1]].setActiveColor();
+          }
+        }else{
+          this.board[el[0]][el[1]].setActiveColor();
+        }
+      }
+    })
   }
 }
--- src/Pieces/Queen.js
+++ src/Pieces/Queen.js
@@ -12,4 +12,124 @@ export default class Queen extends Piece {
     }
     this.side = side;
   }
+  showCorrectCell() {
+
+    var x = this.boardX;
+    var y = this.boardY;
+
+    // up left
+    var j = y - 1;
+    for (var i = x - 1; i >= 0; i--) {
+      if (j > 7 || j < 0) {
+        break;
+      }
+      if (this.isPiece(i, j)) {
+        if (!this.isAlly(i, j)) {
+          this.board[i][j].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][j].setActiveColor();
+      j--;
+    }
+
+    // up right
+    j = y - 1;
+    for (var i = x + 1; i <= 7; i++) {
+      if (j > 7 || j < 0) {
+        break;
+      }
+      if (this.isPiece(i, j)) {
+        if (!this.isAlly(i, j)) {
+          this.board[i][j].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][j].setActiveColor();
+      j--;
+    }
+
+    // down left
+    j = y + 1;
+    for (var i = x - 1; i >= 0; i--) {
+      if (j > 7 || j < 0) {
+        break;
+      }
+      if (this.isPiece(i, j)) {
+        if (!this.isAlly(i, j)) {
+          this.board[i][j].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][j].setActiveColor();
+      j++;
+    }
+
+    // down right
+    j = y + 1;
+    for (var i = x + 1; i <= 7; i++) {
+      if (j > 7 || j < 0) {
+        break;
+      }
+      if (this.isPiece(i, j)) {
+        if (!this.isAlly(i, j)) {
+          this.board[i][j].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][j].setActiveColor();
+      j++;
+    }
+
+     // right
+     for(var i = x+1; i<=7; i++){
+      if(this.isPiece(i, y)){
+        if(!this.isAlly(i, y)){
+          this.board[i][y].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][y].setActiveColor();
+    }
+    // left
+    for(var i = x-1; i>=0; i--){
+      if(this.isPiece(i, y)){
+        if(!this.isAlly(i, y)){
+          this.board[i][y].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[i][y].setActiveColor();
+    }
+
+    // down
+    for(var i = y+1; i<=7; i++){
+      if(this.isPiece(x, i)){
+        if(!this.isAlly(x, i)){
+          this.board[x][i].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[x][i].setActiveColor();
+    }
+
+    // up
+    for(var i = y-1; i>=0; i--){
+      if(this.isPiece(x, i)){
+        if(!this.isAlly(x, i)){
+          this.board[x][i].setActiveColor();
+          break;
+        }
+        break;
+      }
+      this.board[x][i].setActiveColor();
+    }
+  }
 }
